5160d0fd7aaeee4769813558d231bf526a66091b
Furkan Simsek <46837759+simsekfurkan@users.noreply.github.com>
2021-11-19 20:14:02 +0100

    Create odev_3.sql

---
+++ odev_3.sql
@@ -0,0 +1,17 @@
+
+--select country from country
+--Where country like 'A%a'
+
+
+--select country from country
+--Where country like '%_____n'
+
+
+
+--select title from film
+--Where title ilike '%t%t%t%t%'
+
+
+select * from film 
+Where title like 'C%' and length >90 and rental_rate = 2.99 
+
