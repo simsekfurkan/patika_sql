4569c2d8aeeb54ccf239aaa1608e6779d8a9d8b3
Furkan Simsek <46837759+simsekfurkan@users.noreply.github.com>
2021-11-20 15:24:02 +0100

    Create odev_5.sql

---
+++ odev_5.sql
@@ -0,0 +1,17 @@
+--Select title, length from film 
+--where title
+--like '%n'  order by length desc  limit 5 ;
+
+
+--select title, length from film 
+--where title
+--like '%n' order by length 
+--offset 5 
+--limit 5
+
+
+select last_name,store_id from customer 
+where store_id = 1
+order by last_name limit 4 
+
+
