24cade11923cb1eabb8c0354608bcca70b3dff31
Furkan Simsek <46837759+simsekfurkan@users.noreply.github.com>
2021-11-20 22:54:34 +0100

    Create ödev_6.sql

---
+++ ödev_6.sql
@@ -0,0 +1,10 @@
+--select avg(rental_rate) from film
+
+--select count(title) from film
+--where title like 'C%'
+
+--select max(length) from film 
+--where rental_rate = 0.99 
+
+SELECT count(DISTINCT replacement_cost) from film Where
+length > 150 
